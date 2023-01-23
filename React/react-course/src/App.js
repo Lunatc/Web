ce3c3576e84303acba653fee8d871576de556b6c
Lunatc <cintia.braz@unesp.br>
2023-01-23 09:02:41 -0300

    Created React learning project

---
+++ React/react-course/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
